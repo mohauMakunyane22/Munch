c9cf351c5098da77a25627e4d0fd421f3a388ca6
mohauMakunyane22 <2664333@students.wits.ac.za>
2025-12-07 19:34:33 +0200

    created header and backend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 //import logo from "./logo.svg";
 import "./App.css";
+import AppRoutes from "./AppRoutes";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <AppRoutes />
     </>
   );
 }
